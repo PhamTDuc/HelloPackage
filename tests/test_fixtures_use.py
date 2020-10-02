c2bdf1bd9cb33a398d60690ba5025da175491281
Phamduc <phamduc0701@gmail.com>
2020-10-02 23:34:59 +0700

    + Add tests

---
+++ tests/test_fixtures_use.py
@@ -0,0 +1,20 @@
+import pytest
+import hello_package
+
+
+def test_compareA(supplier):
+    z = 35
+    assert supplier[0] == z, "a and z comparison failed"
+
+
+def test_compareB(supplier):
+    z = 35
+    assert supplier[1] == z, "b and z comparison failed"
+
+
+def test_compareC(supplier):
+    z = 35
+    assert supplier[2] == z, "c and z comparison failed"
+
+
+hello_package.hello()
